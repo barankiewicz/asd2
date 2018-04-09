2e632c39c4c5135ad53f0764a5dc0e0d70ab8167
Filip Barankiewicz <barankiewicz@protonmail.ch>
2018-04-09 16:49:49 +0200

    Improvements on lab7

--- asd2-lab7/lab7/WorkManager.cs
+++ asd2-lab7/lab7/WorkManager.cs
@@ -5,6 +5,9 @@ namespace ASD
 {
     public class WorkManager : MarshalByRefObject
     {
+        int difference = int.MaxValue;
+        int[] minResult;
+
         /// <summary>
         /// Implementacja wersji 1
         /// W tablicy blocks zapisane s¹ wagi wszystkich bloków do przypisania robotnikom.
@@ -17,8 +20,6 @@ namespace ASD
         /// </summary>
         public int[] DivideWorkersWork(int[] blocks, int expectedBlockSum)
         {
-            if (blocks.Length < 2)
-                return null;
             int[] result = new int[blocks.Length];
 
             if (!problemSolver(blocks, result, 0, expectedBlockSum, expectedBlockSum))
@@ -33,10 +34,64 @@ namespace ASD
         /// </summary>
         public int[] DivideWorkWithClosestBlocksCount(int[] blocks, int expectedBlockSum)
         {
-            return new int[0];
+            difference = int.MaxValue;
+            minResult = new int[blocks.Length];
+            int[] result = new int[blocks.Length];
+            if (blocks.Sum() < 2 * expectedBlockSum)
+                return null;
+
+            problemSolverSec(blocks, result, 0, expectedBlockSum, expectedBlockSum, 0, 0);
+
+            int sum = minResult.Sum();
+            if (sum == 0)
+                return null;
+            else
+                return minResult;
         }
 
-        // Mo¿na dopisywaæ pola i metody pomocnicze
+        bool problemSolverSec(int[] blocks, int[] result, int startingPoint, int remainingSumA, int remainingSumB, int blockCountA, int blockCountB)
+        {
+            //Znalezione poprawne mapowanie
+            if (remainingSumA == 0 && remainingSumB == 0)
+            {
+                //Jesli mniejsze, nalezy nadpisac minimalne rozwiazane
+                if (Math.Abs(blockCountA - blockCountB) < difference)
+                {
+                    difference = Math.Abs(blockCountA - blockCountB);
+                    result.CopyTo(minResult, 0);
+                    return false;
+                }
+
+                //Jesli 0, nic lepszego juz sie nie znajdzie
+                if (difference == 0)
+                    return true;
+            }
+            //Jesli startingPoint wychodzi poza tablice
+            if (startingPoint >= blocks.Length)
+                return false;
+            //Jesli pozostala suma jest ujemna
+            if (remainingSumA < 0 || remainingSumB < 0)
+                return false;
+
+            result[startingPoint] = 1;
+            if (problemSolverSec(blocks, result, startingPoint + 1, remainingSumA - blocks[startingPoint], remainingSumB, blockCountA + 1, blockCountB))
+                return true;
+            else
+            {
+                result[startingPoint] = 2;
+                if (problemSolverSec(blocks, result, startingPoint + 1, remainingSumA, remainingSumB - blocks[startingPoint], blockCountA, blockCountB + 1))
+                    return true;
+                else
+                {
+                    result[startingPoint] = 0;
+                    if (problemSolverSec(blocks, result, startingPoint + 1, remainingSumA, remainingSumB, blockCountA, blockCountB))
+                        return true;
+                    else
+                        return false;
+                }
+            }
+        }
+
         bool problemSolver(int[] blocks, int[] result, int startingPoint, int remainingSumA, int remainingSumB)
         {
             if (remainingSumA == 0 && remainingSumB == 0)
